4d8bb3b3c51adb045211348fee9046e8e9ba4557
Gangatharan S <122142202+Gangatharan2108@users.noreply.github.com>
2024-05-16 18:04:51 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p><a href="#" style="background-color:skyblue;color:black;">TMS</a></p>
+</div>	
---
+++ admin/includes/header.php
@@ -0,0 +1,31 @@
+<div class="header-main">
+					<div class="logo-w3-agile" style="background: #22beef;">
+								<h1 ><a href="dashboard.php" style="background: #22beef;">Tourism Management System</a></h1>
+							</div>
+				
+						
+						<div class="profile_details w3l">		
+								<ul>
+									<li class="dropdown profile_details_drop">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+											<div class="profile_img">	
+												<span class="prfil-img"><img src="images/User-icon.png" alt=""> </span> 
+												<div class="user-name">
+													<p>Welcome</p>
+													<span>Administrator</span>
+												</div>
+												<i class="fa fa-angle-down"></i>
+												<i class="fa fa-angle-up"></i>
+												
+										</a>
+										<ul class="dropdown-menu drp-mnu">
+											<li> <a href="profile.php" style="background-color:skyblue;"><i class="fa fa-lock"></i> Profile</a> </li> 
+											<li> <a href="change-password.php" style="background-color:skyblue;"><i class="fa fa-lock"></i> Setting</a> </li> 
+											<li> <a href="logout.php" style="background-color:skyblue;"><i class="fa fa-sign-out"></i> Logout</a> </li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+							
+				     <div class="clearfix"> </div>	
+				</div>
